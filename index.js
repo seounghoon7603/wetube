cfdda5377f21ad5db4b6ceae3872bfa184ee3534
psh7603zzz <psh7603zz@gmail.com>
2020-06-15 23:05:42 +0900

    between 두번됨..

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-console.log("hi!");
+import express from "express"
+const app = express();
+
+const PORT = 4000;
+
+const handleListening = () => {
+    console.log(`Listening on: http://localhost:${PORT}`);
+}
+
+const handleHome = (req, res) => {
+    
+    res.send('Hello From Home');
+}
+
+const handleProfile = (req,res) => { 
+    res.send("You are on My profile!!!");
+}
+
+const betweenHome = (req, res, next) => {
+    console.log("between");
+    next();
+}
+
+app.use(betweenHome);
+
+app.get("/",handleHome);
+
+app.get("/profile", handleProfile);
+
+app.listen(PORT, handleListening);
