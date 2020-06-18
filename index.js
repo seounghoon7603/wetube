0d025c71a939cad4bbb7cd8ba33dd3e9b9466ae7
psh7603zz <psh7603zz@kakao.com>
2020-06-18 22:49:10 +0900

    2020-06-18 목요일 2-7강까지 완료

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 import express from "express"
+import morgan from "morgan" // 로깅을 해준다.
+import helmet from "helmet" // 보안
+import cookieParser from "cookie-parser" // 쿠키와 세션 다룸
+import bodyParser from "body-parser" // 바디 다룸
 const app = express();
 
 const PORT = 4000;
@@ -8,7 +12,6 @@ const handleListening = () => {
 }
 
 const handleHome = (req, res) => {
-    
     res.send('Hello From Home');
 }
 
@@ -21,9 +24,20 @@ const betweenHome = (req, res, next) => {
     next();
 }
 
-app.use(betweenHome);
+const betweenRedirect = (req, res, next) => {
+    res.redirect("/");
+}
+
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(helmet());
+app.use(morgan("dev"));
+
 
 app.get("/",handleHome);
+
+app.use(betweenRedirect);
 
 app.get("/profile", handleProfile);
 
